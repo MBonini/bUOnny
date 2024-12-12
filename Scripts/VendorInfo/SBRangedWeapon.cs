4f9a6a35dfb3deda9e4263775ab0f099cca591e7
MBonini <manuel.bonini@gmail.com>
2024-12-12 13:53:47 +0100

    Change champions config & lower price of arrows and bolts

--- Scripts/VendorInfo/SBRangedWeapon.cs
+++ Scripts/VendorInfo/SBRangedWeapon.cs
@@ -38,9 +38,9 @@ namespace Server.Mobiles
                     Add(new GenericBuyInfo(typeof(RepeatingCrossbow), 46, 20, 0x26C3, 0));
                     Add(new GenericBuyInfo(typeof(CompositeBow), 45, 20, 0x26C2, 0));
                 }
-                Add(new GenericBuyInfo(typeof(Bolt), 2, Utility.Random(30, 60), 0x1BFB, 0, true));
+                Add(new GenericBuyInfo(typeof(Bolt), 1, 999, 0x1BFB, 0, true));
                 Add(new GenericBuyInfo(typeof(Bow), 40, 20, 0x13B2, 0));
-                Add(new GenericBuyInfo(typeof(Arrow), 2, Utility.Random(30, 60), 0xF3F, 0, true));
+                Add(new GenericBuyInfo(typeof(Arrow), 1, 999, 0xF3F, 0, true));
                 Add(new GenericBuyInfo(typeof(Feather), 2, Utility.Random(30, 60), 0x1BD1, 0, true));
                 Add(new GenericBuyInfo(typeof(Shaft), 3, Utility.Random(30, 60), 0x1BD4, 0, true));
             }
@@ -53,11 +53,11 @@ namespace Server.Mobiles
                 Add(typeof(Bolt), 1);
                 Add(typeof(Arrow), 1);
                 Add(typeof(Shaft), 1);
-                Add(typeof(Feather), 1);			
+                Add(typeof(Feather), 1);
 
                 Add(typeof(HeavyCrossbow), 27);
                 Add(typeof(Bow), 17);
-                Add(typeof(Crossbow), 25); 
+                Add(typeof(Crossbow), 25);
 
                 if (Core.AOS)
                 {
